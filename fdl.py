6e5f33c22e1cce28cc559fa95943cb85779fd227
Michael Struwig <michael@praelexis.com>
2019-06-09 17:54:12 +0200

    Parameterize chunk size. Add some help.

--- fdl.py
+++ fdl.py
@@ -1,3 +1,4 @@
+
 """A minimal CLI download utility."""
 import click
 import requests
@@ -5,7 +6,7 @@ from tqdm import tqdm
 import warnings
 
 
-def download(url):
+def download(url, chunk_size=4096):
     """Download a file from `url` and stream/save it to disk."""
     local_path = url.split('/')[-1]
     try:
@@ -13,8 +14,8 @@ def download(url):
             try:
                 total_length = int(r.headers.get('content-length'))
             except TypeError:
-                warnings.warn(('Unable to get file size. Are you sure the URL
-                               is correct?'),
+                warnings.warn('Unable to get file size.\
+                Are you sure the URL is correct?',
                               stacklevel=2)
                 total_length = 0
             with tqdm(total=total_length,
@@ -22,7 +23,7 @@ def download(url):
                       unit_scale=True,
                       unit_divisor=1024) as pbar:
                 with open(local_path, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=4*1024):
+                    for chunk in r.iter_content(chunk_size=chunk_size):
                         if chunk:
                             f.write(chunk)
                             f.flush()
@@ -36,13 +37,24 @@ def download(url):
 
 @click.command()
 @click.argument('urls', required=False, nargs=-1)
-def download_files(urls):
+@click.option('--chunk-size', default=4096, help='Set the chunk size in bytes. Default is 4096.')
+def download_files(urls, chunk_size):
+    """Download a list of URLs to the current directory. FDL also accepts piped
+    values - see the examples.
+
+    \b
+    Examples:
+        python fdl.py url1 url2 url3
+        echo 'url-to-download' | python fdl.py
+        cat urls.txt | python fdl.py
+
+    """
     if not urls:  # If not manually specified
         # See if the user piped in the urls
         urls = click.get_text_stream('stdin').readlines()
     urls = [url.strip() for url in urls]
     for url in urls:
-        download(url)
+        download(url, chunk_size)
 
 
 if __name__ == '__main__':
